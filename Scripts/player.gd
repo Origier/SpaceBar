61538dfa73b6251c758b8e3f55186e165484f353
Aaron <sergeantbeast456@gmail.com>
2025-06-29 11:01:28 -0400

    rework: changed the game to be a multi-button game to make it more responsive.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -9,9 +9,6 @@ extends CharacterBody2D
 # The required downward velocity to allow jumping to continue
 @export var falling_speed_requirement := 200
 
-# Time in seconds to clear the buffer and use the data provided to make a move
-@export var buffer_refresh_time := 0.075
-var input_buffer := []
 # Cooldown timer for the charged jump in seconds
 @export var hold_jump_cooldown = 10
 var hold_jump_available := true
@@ -20,22 +17,12 @@ var hold_timer := 0.0
 # Flag to determine if to add hold time or not
 var hold_flag := false
 
-# Keyboard code for the spacebar
-var SPACE_BAR_CODE = 32
-
 # State machine to control how the player operates in different states
 enum STATE {
 	IDLE,
 	JUMPING,
 	FALLING,
 	FALLING_FAST
-}
-
-# States for the space bar to track for input buffering
-enum SPACE_BAR_STATE {
-	PRESS,
-	HOLD,
-	PRESS_AND_RELEASE
 }
 
 # State of the state machine
@@ -53,89 +40,48 @@ func update_state():
 
 func _ready():
 	current_state = STATE.IDLE
-	$InputBufferTimer.wait_time = buffer_refresh_time
 	$HoldJumpTimer.wait_time = hold_jump_cooldown
 	
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	update_state()
-	move_and_slide()
-	# Tracking if the player is holding the space bar
-	if hold_flag and hold_jump_available:
-		velocity.y = 0
-		hold_timer += delta
-
-# Flushes the input buffer while processing the input
-func process_input_buffer():
-	# Input buffer based on size
-	# Logic for held jump
-	if len(input_buffer) >= 2 and input_buffer[1] == SPACE_BAR_STATE.HOLD and hold_jump_available:
+	var correct_state = current_state == STATE.IDLE or current_state == STATE.FALLING_FAST
+	if Input.is_action_just_pressed("jump_left") and correct_state:
+		current_state = STATE.JUMPING
+		velocity = Vector2.ZERO
+		velocity.y = jump_speed
+		velocity.x = -dash_speed
+	elif Input.is_action_just_pressed("jump_right") and correct_state:
+		current_state = STATE.JUMPING
+		velocity = Vector2.ZERO
+		velocity.y = jump_speed
+		velocity.x = dash_speed
+	elif Input.is_action_pressed("hold_jump"):
+		hold_flag = true
+	elif Input.is_action_just_released("hold_jump") and hold_flag:
 		current_state = STATE.JUMPING
 		velocity = Vector2.ZERO
 		velocity.y = jump_speed * hold_timer * hold_scalar
 		hold_timer = 0.0
 		hold_jump_available = false
 		$HoldJumpTimer.start()
-	# Logic for the other combos
-	else:
-		var correct_state = current_state == STATE.IDLE or current_state == STATE.FALLING_FAST
-		# Ensure the player is in the right state before applying any jumps
-		if not correct_state:
-			input_buffer = []
-			pass
-		# Jump Logic
-		if len(input_buffer) == 1 and input_buffer[0] == SPACE_BAR_STATE.PRESS_AND_RELEASE:
-			current_state = STATE.JUMPING
-			velocity = Vector2.ZERO
-			velocity.y = jump_speed
-		# Logic for jumping to the right
-		elif len(input_buffer) == 2 and input_buffer[1] == SPACE_BAR_STATE.PRESS_AND_RELEASE:
-			current_state = STATE.JUMPING
-			velocity = Vector2.ZERO
-			velocity.y = jump_speed
-			velocity.x = dash_speed
-		# Logic for jumping to the left
-		elif len(input_buffer) >= 3 and input_buffer[2] == SPACE_BAR_STATE.PRESS_AND_RELEASE:
-			current_state = STATE.JUMPING
-			velocity = Vector2.ZERO
-			velocity.y = jump_speed
-			velocity.x = -dash_speed
-	input_buffer = []
+	elif Input.is_action_just_pressed("basic_jump") and correct_state:
+		current_state = STATE.JUMPING
+		velocity = Vector2.ZERO
+		velocity.y = jump_speed
+	move_and_slide()
+	# Tracking if the player is holding the space bar
+	if hold_flag and hold_jump_available:
+		velocity.y = 0
+		hold_timer += delta
 
-# Handles all key press events and dispatches accordingly
-func handle_key_press_event(event):
-	match event.keycode:
-		# Capture the press and release to determine the start of input
-		SPACE_BAR_CODE:
-			# Each key press resets the timer - allowing for further key presses to register
-			var pressed = event.is_pressed()
-			var repeating = event.is_echo()
-			if pressed:
-				$InputBufferTimer.stop()
-				input_buffer.append(SPACE_BAR_STATE.PRESS)
-				if repeating:
-					hold_flag = true
-					input_buffer[-1] = SPACE_BAR_STATE.HOLD
-			else:
-				$InputBufferTimer.start()
-				hold_flag = false
-				input_buffer[-1] = SPACE_BAR_STATE.PRESS_AND_RELEASE
-
-# Main function for handling events in the game due to sporadic physics type events
-func _unhandled_input(event):
-	if event is InputEventKey:
-		handle_key_press_event(event)
 
 func _on_idle_timer_timeout() -> void:
 	# check again to validate the player has stopped moving
 	if velocity == Vector2.ZERO or is_on_floor():
 		current_state = STATE.IDLE
 		velocity.x = 0
-
-# Calls the process function to handle the input buffer
-func _on_input_buffer_timer_timeout() -> void:
-	process_input_buffer()
 
 # Resets the hold jump ability
 func _on_hold_jump_timer_timeout() -> void:
